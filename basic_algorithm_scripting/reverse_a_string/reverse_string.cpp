5e43d8dfefee90f97e32302c5d1c96047b3626c4
A Tan <37970614+nguyenatan@users.noreply.github.com>
2018-05-04 17:35:03 +0700

    Create reverse_string.cpp

---
+++ basic_algorithm_scripting/reverse_a_string/reverse_string.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	string str;
+	getline(cin, str);
+	
+	reverse(str.begin(), str.end());
+	cout << str << "\n";
+	
+	//cin.get();
+	return 0;
+}
